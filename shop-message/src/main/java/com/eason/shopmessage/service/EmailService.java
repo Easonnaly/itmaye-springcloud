0e9f8ef1b5d28f5cdaa986326f797a6665a9dc2b
Easonnaly <18888046840@163.com>
2019-11-12 00:39:26 +0800

    邮件发送

--- shop-message/src/main/java/com/eason/shopmessage/service/EmailService.java
+++ shop-message/src/main/java/com/eason/shopmessage/service/EmailService.java
@@ -2,19 +2,26 @@ package com.eason.shopmessage.service;
 
 import com.eason.shopmessage.adapter.MessageAdapter;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.annotation.Resource;
+
 //处理第三方发送邮件
 @Slf4j
 @Service
 public class EmailService implements MessageAdapter {
-   
+   @Resource
+   private JavaMailSender javaMailSender;
+
 	@Override
 	public void sendMsg(JSONObject body) {
+
 		// 处理发送邮件
 		String email=body.getString("email");
 		log.info("准备发送邮件");
@@ -22,6 +29,18 @@ public class EmailService implements MessageAdapter {
 			return ;
 		}
 		log.info("消息服务平台发送邮件:{}",email);
+		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+		//来自账号
+		simpleMailMessage.setFrom(email);
+		//发送账号
+		simpleMailMessage.setTo(email);
+		//标题
+		simpleMailMessage.setSubject("蚂蚁课堂提示您，会员成功邮件");
+		//内容
+		simpleMailMessage.setText("恭喜您注册成功！");
+		//发送邮件
+		javaMailSender.send(simpleMailMessage);
+		log.info("邮件发送成功！");
 	}
 
 }
